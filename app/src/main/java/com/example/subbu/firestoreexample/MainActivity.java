b1d2ec22197272a06ee0ff0611f678631b399632
chodisubramanyam <subbu.chodi123@gmail.com>
2017-10-19 13:18:57 +0530

    animated vector drawable added

--- app/src/main/java/com/example/subbu/firestoreexample/MainActivity.java
+++ app/src/main/java/com/example/subbu/firestoreexample/MainActivity.java
@@ -1,33 +1,61 @@
 package com.example.subbu.firestoreexample;
 
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.graphics.drawable.Animatable2Compat;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.ArrayMap;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "response";
     FirebaseFirestore firebaseFirestore;
+    boolean isrotated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView imageView=findViewById(R.id.imageView);
+        imageView.setImageResource(isrotated?R.drawable.animate_arrow_down :R.drawable.animate_arrow_up);
+        TextView textView=findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isrotated = !isrotated;
+
+               // imageView.setImageResource(isrotated?R.drawable.arrow_down:R.drawable.arrow_top);
+                final AnimatedVectorDrawableCompat drawableCompat= (AnimatedVectorDrawableCompat) imageView.getDrawable();
+                drawableCompat.registerAnimationCallback(new Animatable2Compat.AnimationCallback() {
+                    @Override
+                    public void onAnimationEnd(Drawable drawable) {
+                        drawableCompat.clearAnimationCallbacks();
+                        imageView.setImageResource(isrotated?R.drawable.animate_arrow_down :R.drawable.animate_arrow_up);
+                    }
+                });
+                drawableCompat.start();
+//                AnimatedVectorDrawable animatedVectorDrawable= (AnimatedVectorDrawable) imageView.getDrawable();
+                /*if (imageView.getDrawable() instanceof Animatable){
+                    ((Animatable) imageView.getDrawable()).start();
+                }*/
+            }
+        });
         firebaseFirestore=FirebaseFirestore.getInstance();
         User user=new User(22,"Hyderabad","8184870282","Subramanyam");
         User user1=new User(22,"Nizamabad","8328624128","Subramanyam");
